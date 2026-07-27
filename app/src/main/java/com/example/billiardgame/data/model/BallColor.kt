d504391df363050e4d92ee2a5cf7b7660d634d25
ARK933 <130328371+ARK933@users.noreply.github.com>
2026-07-27 11:03:18 +0800

    Update BallColor.kt

--- app/src/main/java/com/example/billiardgame/data/model/BallColor.kt
+++ app/src/main/java/com/example/billiardgame/data/model/BallColor.kt
@@ -1,22 +1,22 @@
 package com.example.billiardgame.data.model
 
-enum class BallColor {
-    CUE(0xFFF5F5F5.toInt()),       // White
-    SOLID_YELLOW(0xFFFFFF00.toInt()),   // 1
-    SOLID_BLUE(0xFF0000FF.toInt()),     // 2
-    SOLID_RED(0xFFFF0000.toInt()),      // 3
-    SOLID_PURPLE(0xFF800080.toInt()),   // 4
-    SOLID_ORANGE(0xFFFF8C00.toInt()),   // 5
-    SOLID_GREEN(0xFF008000.toInt()),    // 6
-    SOLID_MAROON(0xFF800000.toInt()),   // 7
-    EIGHT_BLACK(0xFF000000.toInt()),    // 8
-    STRIPE_YELLOW(0xFFFFFF00.toInt()),  // 9
-    STRIPE_BLUE(0xFF0000FF.toInt()),    // 10
-    STRIPE_RED(0xFFFF0000.toInt()),     // 11
-    STRIPE_PURPLE(0xFF800080.toInt()),  // 12
-    STRIPE_ORANGE(0xFFFF8C00.toInt()),  // 13
-    STRIPE_GREEN(0xFF008000.toInt()),   // 14
-    STRIPE_MAROON(0xFF800000.toInt()),  // 15
+enum class BallColor(val colorInt: Int) {
+    CUE(0xFFF5F5F5.toInt()),              // White
+    SOLID_YELLOW(0xFFFFFF00.toInt()),     // 1
+    SOLID_BLUE(0xFF0000FF.toInt()),       // 2
+    SOLID_RED(0xFFFF0000.toInt()),        // 3
+    SOLID_PURPLE(0xFF800080.toInt()),     // 4
+    SOLID_ORANGE(0xFFFF8C00.toInt()),     // 5
+    SOLID_GREEN(0xFF008000.toInt()),      // 6
+    SOLID_MAROON(0xFF800000.toInt()),     // 7
+    EIGHT_BLACK(0xFF000000.toInt()),      // 8
+    STRIPE_YELLOW(0xFFFFFF00.toInt()),    // 9
+    STRIPE_BLUE(0xFF0000FF.toInt()),      // 10
+    STRIPE_RED(0xFFFF0000.toInt()),       // 11
+    STRIPE_PURPLE(0xFF800080.toInt()),    // 12
+    STRIPE_ORANGE(0xFFFF8C00.toInt()),    // 13
+    STRIPE_GREEN(0xFF008000.toInt()),     // 14
+    STRIPE_MAROON(0xFF800000.toInt());    // 15  <-- 注意这里改成了分号 ';'
 
     companion object {
         fun fromNumber(num: Int): BallColor = when (num) {
