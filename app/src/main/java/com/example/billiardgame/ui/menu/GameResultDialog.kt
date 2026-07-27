93aca800e1b40a04fa05d43015bc2317bb7031c0
ARK933 <130328371+ARK933@users.noreply.github.com>
2026-07-27 15:33:24 +0800

    Update GameResultDialog.kt

--- app/src/main/java/com/example/billiardgame/ui/menu/GameResultDialog.kt
+++ app/src/main/java/com/example/billiardgame/ui/menu/GameResultDialog.kt
@@ -1,5 +1,6 @@
 package com.example.billiardgame.ui.menu
-
+import androidx.compose.ui.window.Dialog
+import androidx.compose.runtime.Composable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
