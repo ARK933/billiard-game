02bf2f5ab5f82ecc97ecac0dee6fee4912106f3c
ARK933 <130328371+ARK933@users.noreply.github.com>
2026-07-27 15:32:06 +0800

    Update PowerMeter.kt

--- app/src/main/java/com/example/billiardgame/ui/game/components/PowerMeter.kt
+++ app/src/main/java/com/example/billiardgame/ui/game/components/PowerMeter.kt
@@ -1,5 +1,5 @@
 package com.example.billiardgame.ui.game.components
-
+import androidx.compose.ui.Alignment
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
